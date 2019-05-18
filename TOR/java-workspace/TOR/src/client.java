26836e4e7d5938a65380131d2909e5e321830d70
MarikinPaulina <felicitaspura@gmail.com>
2019-05-18 11:27:05 +0200

    Cleaning and file with nodes addresses

--- TOR/java-workspace/TOR/src/client.java
+++ TOR/java-workspace/TOR/src/client.java
@@ -1,12 +1,11 @@
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+
 import org.json.simple.JSONObject;
 
 
@@ -27,24 +26,36 @@ public class client {
             }
         }
 
-        ArrayList<node_data> nodes = new ArrayList<node_data>();
-        nodes.add(new node_data("",501));
-        nodes.add(new node_data("",666));
-        nodes.add(new node_data("",759));
-        nodes.add(new node_data("",812));
-        nodes.add(new node_data("",1024));
+//        ArrayList<node_data> nodes = new ArrayList<node_data>();
+//        nodes.add(new node_data("",501));
+//        nodes.add(new node_data("",666));
+//        nodes.add(new node_data("",759));
+//        nodes.add(new node_data("",812));
+//        nodes.add(new node_data("",1024));
+//        Collections.shuffle(nodes);
+
+        List<List<String>> nodes = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("nodes.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                nodes.add(Arrays.asList(values));
+            }
+        }
         Collections.shuffle(nodes);
+
+
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Podaj wiadomość:");
         String message = sc.nextLine();
         System.out.println("Podaj adresata:");
-        node_data target = nodes.get(Integer.parseInt(sc.nextLine()));
-
-        nodes = (ArrayList<node_data>) nodes.subList(0,N);
-        nodes.add(target);
-
-        node_data next = nodes.remove(0);
+//        node_data target = nodes.get(Integer.parseInt(sc.nextLine()));
+//
+//        nodes = (ArrayList<node_data>) nodes.subList(0,N);
+//        nodes.add(target);
+//
+//        node_data next = nodes.remove(0);
 
         JSONObject obj = new JSONObject();
         obj.put("forward",1);
@@ -54,8 +65,8 @@ public class client {
 
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket sentPacket = new DatagramPacket(send_data, send_data.length);
-        sentPacket.setAddress(next.IP);
-        sentPacket.setPort(next.port);
+//        sentPacket.setAddress(next.IP);
+//        sentPacket.setPort(next.port);
         socket.send(sentPacket);
     }
 
